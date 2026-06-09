e513c15d64dabc8cb8ce62692c9ecb2b126366f4
arrowcat <50666861+Lionel-Messi-10@users.noreply.github.com>
2026-06-08 20:53:44 -0700

    Clean up comments in calibrate
    Removed commented code for clarity.

--- calibrate.py
+++ calibrate.py
@@ -12,17 +12,14 @@ HOLD_ROI = {'top': 310, 'left': 880, 'width': 80, 'height': 80}
 def calibrate():
     with mss() as sct:
         while True:
-            # Capture the whole screen or specific areas
             board_img = np.array(sct.grab(BOARD_ROI))
             next_img = np.array(sct.grab(NEXT_ROI))
             hold_img = np.array(sct.grab(HOLD_ROI))
 
-            # Convert BGRA to BGR for OpenCV
             board_img = cv2.cvtColor(board_img, cv2.COLOR_BGRA2BGR)
             next_img = cv2.cvtColor(next_img, cv2.COLOR_BGRA2BGR)
             hold_img = cv2.cvtColor(hold_img, cv2.COLOR_BGRA2BGR)
 
-            # Show the windows
             cv2.imshow('Board Scan Area', board_img)
             cv2.imshow('Next Queue Area', next_img)
             cv2.imshow('Hold Area', hold_img)
